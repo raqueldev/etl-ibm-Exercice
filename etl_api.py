97df3f3c71f80b6b0636c90927f8a175370b09d4
raquel <rvmederos@gmail.com>
2022-07-13 21:09:09 +0200

    [ETL] - Second step - ETL project IMB certification

--- etl_api.py
+++ etl_api.py
@@ -1,9 +1,11 @@
+import json
+
 import requests
 import pandas as pd
-from common import get_config
 
-# URL = 'https://api.apilayer.com/exchangerates_data/live?base=USD&symbols=EUR&apikey=' + config["apiKey"]["api_key"]
+URL = "https://api.apilayer.com/exchangerates_data/latest?base=EUR&apikey="
 
 if __name__ == '__main__':
-    config = get_config()
-    print config["apiKey"]["api_key"]
+    exchanges_rates = requests.get(URL).json()
+    data = pd.DataFrame(exchanges_rates)
+    print data[['rates']]
